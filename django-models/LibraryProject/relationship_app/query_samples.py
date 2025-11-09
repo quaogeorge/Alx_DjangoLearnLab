90f9ce66ee3e8c4d47b060d25c845c7d3e51b485
Kojo Quao <quaogeorge@icloud.com>
2025-11-09 22:33:18 +0000

    Added relationship_app demonstrating ORM relationships

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,13 +1,9 @@
-from relationship_app.models import Author, Book, Library
+from relationship_app.models import Author, Book, Library, Librarian
 
 
 def query_books_by_author(author_name):
-    # Get the author object
     author = Author.objects.get(name=author_name)
-    
-    # Explicitly filter using the ForeignKey
     books = Book.objects.filter(author=author)
-    
     print(f"Books by {author_name}:")
     for book in books:
         print(f"- {book.title}")
@@ -16,7 +12,6 @@ def query_books_by_author(author_name):
 def list_books_in_library(library_name):
     library = Library.objects.get(name=library_name)
     books = library.books.all()
-    
     print(f"Books in {library_name}:")
     for book in books:
         print(f"- {book.title}")
@@ -24,7 +19,7 @@ def list_books_in_library(library_name):
 
 def get_librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    librarian = library.librarian
+    librarian = Librarian.objects.get(library=library)
     print(f"Librarian for {library_name}: {librarian.name}")
 
 
