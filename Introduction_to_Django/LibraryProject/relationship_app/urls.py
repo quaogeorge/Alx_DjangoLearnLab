0a8d891b4b3438d9b309ff6d23b3d49985358321
Kojo Quao <quaogeorge@icloud.com>
2025-11-09 23:36:43 +0000

    Added function-based and class-based views for books and libraries

---
+++ Introduction_to_Django/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('books/', views.list_books, name='list_books'),
+]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import View
+from .models import Book, Library
 
-# Create your views here.
+
+# --- Function-Based View ---
+def list_books(request):
+    books = Book.objects.select_related('author').all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+
+# --- Class-Based View ---
+class LibraryDetailView(View):
+    def get(self, request, pk):
+        library = get_object_or_404(Library, pk=pk)
+        return render(request, 'relationship_app/library_detail.html', {'library': library})
