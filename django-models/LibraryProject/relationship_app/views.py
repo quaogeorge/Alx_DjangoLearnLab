83407201fb28ecca96e5e40ab1889842dd52b891
Kojo Quao <quaogeorge@icloud.com>
2025-11-14 17:26:00 +0000

    Django Views and Config

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,3 +5,12 @@ from .models import Book
 def list_books(request):
     books = Book.objects.select_related('author').all()
     return render(request, "list_books.html", {"books": books})
+
+from django.views.generic import DetailView
+from .models import Library
+
+# Class-based view
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "library_detail.html"
+    context_object_name = "library"
