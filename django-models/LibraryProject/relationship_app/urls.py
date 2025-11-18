1e709df875ec1430f9b340310fda99f8301c6641
Kojo Quao <quaogeorge@icloud.com>
2025-11-18 19:54:01 +0000

    Django Views and URL Configuration 03

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from .views import list_books
+from .views import add_book, edit_book, delete_book
 app_name = 'relationship_app'
 
 urlpatterns = [
