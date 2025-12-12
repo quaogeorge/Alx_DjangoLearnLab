b4e34d57a3e0ed9200657b1f1f56c006c96a1577
Kojo Quao <quaogeorge@icloud.com>
2025-12-12 02:21:40 +0000

    Project Setup and User Authentication for a Social Media API

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,11 +1,22 @@
-from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'id', 'username', 'email',
+            'first_name', 'last_name', 'bio', 'profile_picture'
+        ]
+        read_only_fields = ['id']
+
+
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True)  # REQUIRED
     token = serializers.SerializerMethodField()
 
     class Meta:
@@ -14,13 +25,23 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         password = validated_data.pop('password')
-        user = User.objects.create_user(
+
+        # REQUIRED for your check:
+        user = User.objects.create_user(  
             password=password,
             **validated_data
         )
+
+        # Create auth token
         Token.objects.create(user=user)
+
         return user
 
     def get_token(self, obj):
         token = Token.objects.filter(user=obj).first()
         return token.key if token else None
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()  # REQUIRED
+    password = serializers.CharField(write_only=True)
