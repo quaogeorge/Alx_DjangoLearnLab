229a2952ec1a6f5a46d26ef9d3ce0d7931902f08
Kojo Quao <quaogeorge@icloud.com>
2025-12-12 02:04:32 +0000

    Project Setup and User Authentication for a Social Media API

--- social_media_api/accounts/serializer.py
+++ social_media_api/accounts/serializer.py
@@ -5,26 +5,16 @@ from rest_framework.authtoken.models import Token
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
-    followers_count = serializers.SerializerMethodField()
-    following_count = serializers.SerializerMethodField()
-    profile_picture = serializers.ImageField(required=False)
-
     class Meta:
         model = User
         fields = [
             'id', 'username', 'email', 'first_name', 'last_name',
-            'bio', 'profile_picture', 'followers_count', 'following_count'
+            'bio', 'profile_picture'
         ]
-        read_only_fields = ['id', 'followers_count', 'following_count']
-
-    def get_followers_count(self, obj):
-        return obj.followers.count()
-
-    def get_following_count(self, obj):
-        return obj.following.count()
+        read_only_fields = ['id']
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, min_length=8)
+    password = serializers.CharField(write_only=True)
     token = serializers.SerializerMethodField()
 
     class Meta:
@@ -36,12 +26,12 @@ class RegisterSerializer(serializers.ModelSerializer):
         user = User(**validated_data)
         user.set_password(password)
         user.save()
-        Token.objects.create(user=user)  # create token
+        Token.objects.create(user=user)
         return user
 
     def get_token(self, obj):
-        token_obj = Token.objects.filter(user=obj).first()
-        return token_obj.key if token_obj else None
+        token = Token.objects.filter(user=obj).first()
+        return token.key if token else None
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
